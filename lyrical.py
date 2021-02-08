33801ece01ef40cf5eca742568ee0bd5c22ddd79
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-02-08 12:45:33 +0800

    Lyrical.py for helios 1.2bf1

---
+++ lyrical.py
@@ -0,0 +1,11 @@
+# lyrical.py
+# for finding a song's lyrics
+
+from lyricsgenius import Genius
+TOKEN = ('')#insert token
+genius = Genius(TOKEN)
+
+def find(inputSong,inputArtist):
+    songToSearch = (inputSong,inputArtist)
+    songlyrics = genius.search_song(songToSearch)
+    return songlyrics.lyrics

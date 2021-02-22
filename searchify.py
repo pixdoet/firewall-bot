9415f87e5a65975be5b572f0a5d749c9e8db4515
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-02-22 08:47:17 +0800

    Create searchify.py

---
+++ searchify.py
@@ -0,0 +1,15 @@
+# this is kinda broken, imma fix this after the bot database
+
+from gsearch.googlesearch import search
+
+def apisearchg(query,num):
+    results = search(searchg, num_results=num)
+    return results
+
+def searchg():
+    qr = input("Enter search term: ")
+    ns = input("Enter max results: ")
+    rs = search(qr,num_results = 10)
+    print(rs)
+
+searchg()
